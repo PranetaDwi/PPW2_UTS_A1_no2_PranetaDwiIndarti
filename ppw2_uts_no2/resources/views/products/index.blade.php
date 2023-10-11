06704b66514ae2fd2a9ee5f91682564bc6f6a803
PranetaDwi <pranetadwiindarti2004@mail.ugm.ac.id>
2023-10-11 08:48:52 +0700

    bismiilah fiks

--- ppw2_uts_no2/resources/views/products/index.blade.php
+++ ppw2_uts_no2/resources/views/products/index.blade.php
@@ -35,7 +35,7 @@
                             <td>{{ $product->quantity }}</td>
                             <td>{{ $product->price }}</td>
                             <td>
-                                <form class="bi bi-trash" onclick="return confirm('Do you want to delete this product?');" action="{{ route('products.destroy', $product->id) }}" method="post">
+                                <form  action="{{ route('products.destroy', $product->id) }}" method="post">
                                     @csrf
                                     @method('DELETE')
 
@@ -43,7 +43,7 @@
 
                                     <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square"></i> Edit</a>   
 
-                                    <button type="submit" class="btn btn-danger btn-sm"> Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Do you want to delete this product?')"> Delete</button>
                                 </form>
                             </td>
                         </tr>
